65cd4b4d54eccc07bb0ec567374786dae2956763
swanaik2018 <swanaik2018@gmail.com>
2025-07-24 09:48:45 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -19,21 +19,24 @@ if rate > 0.8:
 return (False, "Charge Rate too high!")
 return (True, "")
 
-def battery_is_ok(temp, soc, rate, reporter=print):
-checks = [
-is_temperature_ok(temp),
-is_soc_ok(soc),
-is_charge_rate_ok(rate)
-]
-for ok, msg in checks:
-if not ok:
-reporter(msg)
+def battery_is_ok(temperature, soc, charge_rate, reporter=print):
+if temperature < 0:
+reporter("Temperature too low!")
+return False
+if temperature > 45:
+reporter("Temperature too high!")
+return False
+if soc < 20:
+reporter("State of Charge too low!")
+return False
+if soc > 80:
+reporter("State of Charge too high!")
+return False
+if charge_rate > 0.8:
+reporter("Charge rate too high!")
 return False
 return True
 
-if __name__ == '__main__':
-assert(battery_is_ok(25, 70, 0.7) is True)
-assert(battery_is_ok(50, 85, 0) is False)
-assert(battery_is_ok(-1, 50, 0.7) is False)
-assert(battery_is_ok(25, 10, 0.7) is False)
-assert(battery_is_ok(25, 50, 0.9) is False)
+if __name__ == "__main__":
+assert battery_is_ok(25, 70, 0.7) is True
+assert battery_is_ok(50, 85, 0) is False 
